cb6e25d128029561a6d97875f68f3237332f512d
Kevin Donnelly <donnelkj@mohio.edu>
2011-07-08 17:32:23 -0400

    Initial Commit

---
+++ CustomNavText/CustomNavTextAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CustomNavTextAppDelegate.h
+//  CustomNavText
+//
+//  Created by Kevin Donnelly on 7/8/11.
+//  Copyright 2011 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomNavTextAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ CustomNavText/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CustomNavText
+//
+//  Created by Kevin Donnelly on 7/8/11.
+//  Copyright 2011 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
